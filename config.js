5c5662af1e9e12a61eea5bffea4507f3de994fba
Yushenka <49005570+Yushenka@users.noreply.github.com>
2026-05-10 21:45:15 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://gaspfbmuellmuoosrymw.supabase.co",
-  supabasePublishableKey: "sb_publishable_YJpxeL1cKfaJBojmczeusQ_ryLLKCW9",
+  supabasePublishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhc3BmYnVtZWxsbXVvb3NyeW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MTkwODUsImV4cCI6MjA5Mzk5NTA4NX0.eHMhzcH5eHhUsCDxVdsM3lnPQQqYaKlaRupQTOj23_w",
   bootstrapFunctionName: "app-bootstrap",
   submitFunctionName: "submit-audit"
 };
